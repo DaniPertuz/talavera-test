a1dfe6c53eb5408eb7ebfd1d6d03e67dadb3dc61
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-03 19:35:41 -0500

    UI Layout HOC to display all content

---
+++ src/presentation/ui/Layout/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { StyleProp, StyleSheet, TextStyle, View } from 'react-native';
+import { appColors } from '../../../theme/colors';
+
+interface Props {
+  children: React.ReactNode;
+  style?: StyleProp<TextStyle>;
+}
+
+export const Layout: FC<Props> = ({ children, style }) => {
+  return (
+    <View testID="layout" style={[styles.container, style]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: appColors.secondary,
+    flex: 1,
+    padding: 20,
+  },
+});
